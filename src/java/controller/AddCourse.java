23a754c65d8b9b92c13af58e86325ca4cbecd9b3
kudo2308 <hieu23082k4@gmail.com>
2025-02-11 11:21:21 +0700

    small change

--- src/java/controller/AddCourse.java
+++ src/java/controller/AddCourse.java
@@ -21,7 +21,7 @@ import java.util.List;
 import java.util.Set;
 import model.Category;
 //gioi han kich thuoc tep
-
+    
 @MultipartConfig(fileSizeThreshold = 1024 * 1024 * 2,
         maxFileSize = 1024 * 1024 * 10,
         maxRequestSize = 1024 * 1024 * 50)
